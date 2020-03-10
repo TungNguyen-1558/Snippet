79c1ec2acf3c7996b151798bb09198cb11cec4f7
Tung Nguyen <tungtnguyen@kms.com.vn>
2020-03-10 13:37:04 +0700

    update field id

--- src/main/java/com/example/snippet/model/Snippet.java
+++ src/main/java/com/example/snippet/model/Snippet.java
@@ -14,7 +14,7 @@ public class Snippet implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(nullable = false, name = "id")
 	private int id;
 
